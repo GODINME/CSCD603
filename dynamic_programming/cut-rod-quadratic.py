d680a6597139a09914dded9fdba897b96cc58787
= <=>
2023-10-27 11:38:09 +0000

    Cutting Rod Buttom-Up or Memoization Solution in Quadrictic Runtime with additional debugging

---
+++ dynamic_programming/cut-rod-quadratic.py
@@ -0,0 +1,29 @@
+# Rod Cutting Buttom-Up or Memoization Solution
+from math import inf 
+
+def cut_rod(p, n):
+    """
+    Let p bet the profit of each rod at length i
+    Let n be the size of the rod from i .. n
+    Let q be the maximum profit for the length n
+    Let r be a list of substructure optimal revenue
+    """
+    r = [None] * (n+1)
+    print("Initial R: ", r)
+    r[0] = 0
+    print("Middle R: ", r)
+
+    for j in range(1, n+1):
+        q = -inf
+        for i in range(j):
+            q = max(q, p[i] + r[j-i-1])
+        r[j] = q
+    print("Last R: ", r)
+    return q
+
+# Example 1
+rod_length = [1, 2, 3, 4]
+rod_profit = [1, 5, 8, 9]
+
+#print("Maximum profit in Example 1 is ", cut_rod(rod_profit, len(rod_profit)))
+print("Maximum profit in Example 1 is ", cut_rod(rod_profit, 4))
